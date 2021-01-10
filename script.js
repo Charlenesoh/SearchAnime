b07b39c7347d52b87c04b0de72329ac4e000c0c6
Charlenesoh <s10204830@connect.np.edu.sg>
2021-01-10 18:33:05 +0800

    changes

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-
-
-
-async function sendApiRequest()[
-    let APP_ID = "4914101"
-    let API_KEY = ""
-    let response  = await fetch ("")
-    console.log(response)
-    let data = await response.json()
-    console.log(data)
-    userAPIData(data)
-]
+let promise = fetch ( "https://api.thedogapi.com/v1/breeds" )
